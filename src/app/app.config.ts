6e4912e92769a411c07ca611b8780ab333fddc1f
Jaysemmm <cortiguerrajcempatrick@gmail.com>
2026-07-03 11:11:36 +0800

    Moving the url to environment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,24 +1,26 @@
-import { ApplicationConfig, inject } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { InMemoryCache, ApolloLink } from '@apollo/client/core';
 import { setContext } from '@apollo/client/link/context';
 import { provideApollo } from 'apollo-angular';
-import  UploadHttpLink  from 'apollo-upload-client/UploadHttpLink.mjs';
+import UploadHttpLink from 'apollo-upload-client/UploadHttpLink.mjs';
 import { routes } from './app.routes';
 import { authInterceptor } from './interceptors/auth.interceptor';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
-    provideApollo(() => {
-      let graphqlUrl = 'http://localhost:8000/graphql';
-      if (typeof window !== 'undefined' && window.location.hostname !== 'localhost' && window.location.hostname !== '127.0.0.1') {
-        graphqlUrl = 'http://nginx/graphql';
-      }
-     const uploadLink = new UploadHttpLink({ uri: graphqlUrl }) as unknown as ApolloLink;
 
+    provideApollo(() => {
+      // 1. Setup the upload link pointing to your dynamic environment URL
+      const uploadLink = new UploadHttpLink({
+        uri: environment.graphqlUrl
+      }) as unknown as ApolloLink;
+
+      // 2. Setup your authentication headers context
       const authLink = setContext((_, { headers }) => {
         const token = sessionStorage.getItem('auth_token');
         return {
@@ -29,6 +31,7 @@ export const appConfig: ApplicationConfig = {
         };
       });
 
+      // 3. Return the merged Apollo configuration
       return {
         link: authLink.concat(uploadLink),
         cache: new InMemoryCache(),
